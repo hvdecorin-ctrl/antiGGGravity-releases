269e582f74a2aa9bcf76fc770fc35d7dbdf7d70f
Kenzo0185 <info.vdt@gmail.com>
2026-02-15 23:02:23 +1300

    commit

--- Commands/General/ViewToggleCommands.cs
+++ Commands/General/ViewToggleCommands.cs
@@ -213,7 +213,7 @@ namespace antiGGGravity.Commands.General
             double h = h_mm * mmToFeet * scale;
             // Increase text offset to avoid clashing (User input: "too close")
             // 50mm seems reasonable for a 1000mm box.
-            double text_offset = 50.0 * mmToFeet * scale; 
+            double text_offset = 200.0 * mmToFeet * scale; 
             double shift = (offset_mm + h_mm) * mmToFeet * scale;
 
             // Geometry Helpers
